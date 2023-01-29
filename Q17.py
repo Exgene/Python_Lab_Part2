5ab1e077fe4e4aecebad1f6bd48892816e3a3473
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2023-01-29 16:10:36 +0530

    Q17 Updated
    Check this out

---
+++ Q17.py
@@ -0,0 +1,31 @@
+
+# read answer key from keys.txt
+with open('keys.txt', 'r') as file:
+    answer_key = file.readline().strip().split(' ')#Each line after space has the answer so we are storing seperately
+
+# read answers from marks.txt
+student_answers=[]
+with open('marks.txt', 'r') as file:
+    for line in file:
+        student_answers.append(line.strip().split(' '))#similarly each line has each students info and asnwers so storing in a list format
+
+
+# calculate score for each student
+scores = []
+for answers in student_answers:#for each student 
+    score = 0
+    for i in range(len(answer_key)):#for each question if answer is true we assign marks (score+=1)
+        if answers[i] == answer_key[i]:
+            score += 1
+    scores.append(score)#appending total marks to the marks list in order
+
+    # display total marks scored by each student
+for i in range(len(scores)):#optional if u want u can print all students marks
+    print(f'Student{i}: {scores[i]}')
+
+# find the index of top scorer
+max_index = scores.index(max(scores))
+
+# display top scorer and the top score
+print(f'Top Scorer: Student{max_index} with marks = {max(scores)}')
+
